cd9685ec6b34ae67c734e066462b282dd79be2c4
shivam singh <shivamfacejee@gmail.com>
2021-09-19 14:17:08 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(myapp());
+}
+
+class myapp extends StatelessWidget {
+  const myapp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      
+      home:   Material(
+
+        child:Center(
+    
+         child: Container(
+           child: Text("welcome to flutter"),
+         ),
+       ),
+
+
+      ),
+    );
+  }
+}
